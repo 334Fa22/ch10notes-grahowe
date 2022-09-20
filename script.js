bc53bcfdc0d052c9711cabbf5dac3c4e1de06cfd
grahowe <omgraham02@gmail.com>
2022-09-19 19:16:05 -0500

    Created burpees function

--- script.js
+++ script.js
@@ -4,6 +4,11 @@
   Date: September 20, 2022
 */
 
+//Global variables
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src");
+var descriptionSource = document.getElementById("despsrc");
+
 //Hamburger menu function
 function hamburger(){
   var menu = document.getElementById("menu-links");
@@ -15,4 +20,12 @@ function hamburger(){
     menu.style.display = "block";
     logo.style.display = "none";
   }
+}
+
+//Function to display burpees video
+function burpees(){
+  videoSource.src = "media/burpees.mp4";
+  descriptionSource.src = "media/burpees-descriptions.vtt";
+  video.style.display = "block";
+  video.load();
 }
